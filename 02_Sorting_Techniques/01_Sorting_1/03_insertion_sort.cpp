ee79d8f49e1617576bb1a83c904f6afc1389bcbe
subhadipbhowmik <shubhadipbhowmik02@gmail.com>
2023-09-21 22:56:21 +0530

    insertion sorting

---
+++ 02_Sorting_Techniques/01_Sorting_1/03_insertion_sort.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+void insertion_sort(int array[], int size) {
+  for (int i = 1; i < size; i++) {
+    int current_element = array[i];
+    int j = i - 1;
+    while (j >= 0 && array[j] > current_element) {
+      array[j + 1] = array[j];
+      j -= 1;
+    }
+    array[j + 1] = current_element;
+  }
+}
+
+int main() {
+  int array[] = {5, 3, 2, 1, 4};
+  int size = sizeof(array) / sizeof(array[0]);
+
+  insertion_sort(array, size);
+
+  cout << "The sorted array is: ";
+  for (int i = 0; i < size; i++) {
+    cout << array[i] << " ";
+  }
+  cout << endl;
+
+  return 0;
+}
