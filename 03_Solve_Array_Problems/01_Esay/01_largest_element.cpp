f12490ceb874e92fc34cd3837fe9b9da12651162
subhadipbhowmik <shubhadipbhowmik02@gmail.com>
2023-11-01 17:50:12 +0530

    updated

--- 03_Solve_Array_Problems/01_Esay/01_largest_element.cpp
+++ 03_Solve_Array_Problems/01_Esay/01_largest_element.cpp
@@ -1,8 +1,8 @@
-#include <bits/stdc++.h>
-int largestElement(vector<int> &arr, int n)
+#include <iostream>
+using namespace std;
+int largestElement(int arr[], int n)
 {
-    // Write your code here.
-    int max = 0;
+    int max = INT_MIN;
     for (int i = 0; i < n; i++)
     {
         if (arr[i] > max)
@@ -12,4 +12,10 @@ int largestElement(vector<int> &arr, int n)
     }
 
     return max;
+}
+int main()
+{
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    return 0;
 }
--- 03_Solve_Array_Problems/01_Esay/03_array_sorted_or_not.cpp
+++ 03_Solve_Array_Problems/01_Esay/03_array_sorted_or_not.cpp
@@ -2,18 +2,35 @@
 
 int arraySortedOrNot(int arr[], int n)
 {
-    if (n == 1 || n == 0)
+    // if (n == 1 || n == 0)
+    //     return 1;
+
+    // if (arr[n - 1] < arr[n - 2])
+    //     return 0;
+
+    // return arraySortedOrNot(arr, n - 1);
+
+    if (n <= 1)
+    {
         return 1;
+    }
 
-    if (arr[n - 1] < arr[n - 2])
-        return 0;
+    bool increasing = arr[0] < arr[1];
 
-    return arraySortedOrNot(arr, n - 1);
+    for (int i = 1; i < n - 1; i++)
+    {
+        if ((arr[i] < arr[i + 1] && !increasing) || (arr[i] > arr[i + 1] && increasing))
+        {
+            return 0;
+        }
+    }
+
+    return 1;
 }
 
 int main()
 {
-    int arr[] = {1, 2, 3, 7, 5};
+    int arr[] = {1, 2, 3, 5, 7};
     int n = sizeof(arr) / sizeof(arr[0]);
 
     int result = arraySortedOrNot(arr, n);
